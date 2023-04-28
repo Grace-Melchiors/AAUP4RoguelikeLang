74eda2fb3998af5757b3b5b218c4321b6f66f8c3
Frederik-Melchiors <94599685+Frederik-Melchiors@users.noreply.github.com>
2023-04-28 10:28:51 +0200

    Add files via upload
    Pandoras box

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Antlr_language;
+using Antlr_language.Content;
+using Antlr4.Runtime;
+
+var fileName = "Content\\test.V"; // args[0]
+var fileContents = File.ReadAllText(fileName);
+
+
+AntlrInputStream inputStream = new AntlrInputStream(fileContents);
+var VestaLexer = new VestaLexer(inputStream);
+CommonTokenStream commonTokenStream = new CommonTokenStream(VestaLexer);
+VestaParser vestaParser = new VestaParser(commonTokenStream);
+//vestaParser.AddErrorListener();
+
+var vestaContext = vestaParser.program();
+
+var visitor = new VestaVisitor();
+visitor.Visit((vestaContext));
